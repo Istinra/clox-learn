d946798e59d8e68711b3c0e3fda83ffccfa55d7d
Samuel Hands <forsaken.panda@gmail.com>
2024-09-19 01:25:04 +1000

    chapter 19 fin

--- vm.c
+++ vm.c
@@ -2,8 +2,12 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "compiler.h"
 #include "debug.h"
+#include "memory.h"
+#include "object.h"
 
 VM vm;
 
@@ -14,9 +18,11 @@ static void resetStack() {
 
 void initVM() {
     resetStack();
+    vm.objects = nullptr;
 }
 
 void freeVM() {
+    freeObjects();
 }
 
 static Value peek(int distance) {
@@ -25,6 +31,20 @@ static Value peek(int distance) {
 
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static void runtimeError(const char *format, ...) {
@@ -82,8 +102,6 @@ static InterpretResult run() {
                 }
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
-            case OP_ADD: BINARY_OP(NUMBER_VAL, +);
-                break;
             case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -);
                 break;
             case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *);
@@ -92,7 +110,7 @@ static InterpretResult run() {
                 break;
             case OP_NOT:
                 push(BOOL_VAL(isFalsey(pop())));
-            break;
+                break;
             case OP_NIL: push(NIL_VAL);
                 break;
             case OP_TRUE: push(BOOL_VAL(true));
@@ -105,8 +123,24 @@ static InterpretResult run() {
                 push(BOOL_VAL(valuesEqual(a, b)));
                 break;
             }
-            case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
-            case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+            case OP_GREATER: BINARY_OP(BOOL_VAL, >);
+                break;
+            case OP_LESS: BINARY_OP(BOOL_VAL, <);
+                break;
+            case OP_ADD: {
+                if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                    concatenate();
+                } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                    double b = AS_NUMBER(pop());
+                    double a = AS_NUMBER(pop());
+                    push(NUMBER_VAL(a + b));
+                } else {
+                    runtimeError(
+                        "Operands must be two numbers or two strings.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
